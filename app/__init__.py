f9b6c78df29b21ff776a37588571f38aabb2e45c
Linsiyang <763478801@qq.com>
2019-08-27 16:08:13 +0800

    first

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+#从flask包中导入Flask类
+from flask import Flask
+from config import Config#从config模块导入Config类
+
+from flask_sqlalchemy import SQLAlchemy#从包中导入类
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+#将Flask类的实例 赋值给名为 app 的变量。这个实例成为app包的成员。
+app = Flask(__name__)
+app.config.from_object(Config)
+# app.config['SECRET_KEY'] = "I am a secret, you can't guess."
+
+login = LoginManager(app)
+login.login_view = 'login'
+
+# print('等会谁（哪个包或模块）在使用我：',__name__)
+
+db = SQLAlchemy(app)#数据库对象
+migrate = Migrate(app, db)#迁移引擎对象
+
+from app import routes,models
+#导入一个新模块models，它将定义数据库的结构，目前为止尚未编写
+
