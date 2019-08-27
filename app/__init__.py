2ff8cc51bacd53ad8cdab4f90c8c386a958b517e
Linsiyang <763478801@qq.com>
2019-08-27 18:05:23 +0800

    8-27 18:05

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
+from app import routes,models,errors
 #从flask包中导入Flask类
 from flask import Flask
-from config import Config#从config模块导入Config类
-
 from flask_sqlalchemy import SQLAlchemy#从包中导入类
 from flask_migrate import Migrate
 from flask_login import LoginManager
+
+from config import Config#从config模块导入Config类
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
 
 #将Flask类的实例 赋值给名为 app 的变量。这个实例成为app包的成员。
 app = Flask(__name__)
@@ -19,6 +24,21 @@ login.login_view = 'login'
 db = SQLAlchemy(app)#数据库对象
 migrate = Migrate(app, db)#迁移引擎对象
 
-from app import routes,models
+from app import routes,models,errors
 #导入一个新模块models，它将定义数据库的结构，目前为止尚未编写
+
+if not app.debug:
+    # ...
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
 
--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm#从flask_wtf包中导入FlaskForm类
-from wtforms import StringField,PasswordField,BooleanField,SubmitField#导入这些类
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms import StringField,PasswordField,BooleanField,SubmitField,TextAreaField#导入这些类
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo,Length
 from app.models import User
 
 class LoginForm(FlaskForm):
@@ -26,3 +26,19 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address.')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+    # 验证用户名
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
